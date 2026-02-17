8e6211e448a35f259a4156dd74e7219df5b50048
PL-maksim-LP <89026134+PL-MAKSIM-LP@users.noreply.github.com>
2026-02-17 12:09:48 +0000

    litte fix

--- build.sh
+++ build.sh
@@ -6,29 +6,31 @@ fi
 > result.txt
 echo "$(grep 'model name' /proc/cpuinfo | uniq | awk -F': ' '{print $2}'), $(grep -c '^processor' /proc/cpuinfo) cores" >> result.txt 
 
-count=100
+count=5
+folder="./data/main/"
+
 echo OneThread_O0 >> result.txt 
-g++ main.cpp -O0 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 0 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ main.cpp -O0 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 0 level of optimization" && ./build/app $folder $count && echo
 
 echo OneThread_O1 >> result.txt 
-g++ main.cpp -O1 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 1 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ main.cpp -O1 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 1 level of optimization" && ./build/app $folder $count && echo
 
 echo OneThread_O2 >> result.txt
-g++ main.cpp -O2 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 2 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ main.cpp -O2 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 2 level of optimization" && ./build/app $folder $count && echo
 
 echo OneThread_O3 >> result.txt
-g++ main.cpp -O3 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 3 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ main.cpp -O3 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run application with 3 level of optimization" && ./build/app $folder $count && echo
 
 echo MultithreadThread_O0 >> result.txt 
-g++ mainMultithread.cpp -O0 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 0 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ mainMultithread.cpp -O0 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 0 level of optimization" && ./build/app $folder $count && echo
 
 echo MultithreadThread_O1 >> result.txt 
-g++ mainMultithread.cpp -O1 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 1 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ mainMultithread.cpp -O1 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 1 level of optimization" && ./build/app $folder $count && echo
 
 echo MultithreadThread_O2 >> result.txt 
-g++ mainMultithread.cpp -O2 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 2 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ mainMultithread.cpp -O2 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 2 level of optimization" && ./build/app $folder $count && echo
 
 echo MultithreadThread_O3 >> result.txt 
-g++ mainMultithread.cpp -O3 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 3 level of optimization" && ./build/app ./data/main/ $count && echo
+g++ mainMultithread.cpp -O3 -o ./build/app  `pkg-config --cflags --libs opencv4` && echo "Run Multithread application with 3 level of optimization" && ./build/app $folder $count && echo
 
 python buildHistogram.py 
